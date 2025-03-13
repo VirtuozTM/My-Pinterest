9115869bee1399a5fef93f3d79d2212a0b569a3d
PETIT <armand_petit@outlook.fr>
2025-03-13 11:21:37 +0100

    Ajout des screen pour le router d'expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="home" />
+    </Stack>
+  );
 }
