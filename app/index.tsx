3d0fda12ee00afb44fe15ae5bdb7f1ba55c54060
PETIT <armand_petit@outlook.fr>
2025-03-13 11:21:54 +0100

    creation du screen de bienvenue

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,114 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
+import { hp, wp } from "@/helpers/common";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { theme } from "@/constants/theme";
+import { router } from "expo-router";
 
 const WelcomeScreen = () => {
+  const navigateToHomeScreen = () => {
+    router.navigate("/home");
+  };
+
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <ImageBackground
+        source={require("@/assets/images/welcome.png")}
+        resizeMode="cover"
+        style={styles.bgImage}
+      />
+      <Animated.View entering={FadeInDown.duration(600)} style={{ flex: 1 }}>
+        <LinearGradient
+          colors={[
+            "rgba(255,255,255,0)",
+            "rgba(255,255,255,0.5)",
+            "white",
+            "white",
+          ]}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        />
+        <View style={styles.content}>
+          <Animated.Text
+            entering={FadeInDown.delay(400).springify()}
+            style={styles.title}
+          >
+            Pixels
+          </Animated.Text>
+          <Animated.Text
+            entering={FadeInDown.delay(500).springify()}
+            style={styles.punchline}
+          >
+            Chaque pixel raconte une histoire
+          </Animated.Text>
+          <Animated.View entering={FadeInDown.delay(600).springify()}>
+            <Pressable
+              onPress={navigateToHomeScreen}
+              style={styles.startButton}
+            >
+              <Text style={styles.startText}>Commencer à explorer</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </Animated.View>
     </View>
   );
 };
 
 export default WelcomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  bgImage: {
+    width: wp(100),
+    height: hp(100),
+    position: "absolute",
+  },
+  gradient: {
+    width: wp(100),
+    height: hp(65),
+    bottom: 0,
+    position: "absolute",
+  },
+  content: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    gap: 14,
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold,
+  },
+  punchline: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium,
+  },
+  startButton: {
+    marginBottom: 30,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 40,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+  },
+  startText: {
+    color: theme.colors.white,
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.medium,
+    letterSpacing: 1,
+  },
+});
