0d84b7fbaa56a92dceeb18175573beb3c1841b2c
PETIT <armand_petit@outlook.fr>
2025-03-15 20:03:41 +0100

    Préférence du francais pour la langue lors des recherche via l'API de Pixabay

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 const apiUrl = `https://pixabay.com/api/?key=${API_KEY}`;
 
 const formatUrl = (params: any) => {
-  let url = apiUrl + "&per_page=25&safesearch=true&editors=true";
+  let url = apiUrl + "&per_page=25&safesearch=true&editors=true&lang=fr";
   if (!params) return url;
   let paramKeys = Object.keys(params);
   paramKeys.map((key) => {
