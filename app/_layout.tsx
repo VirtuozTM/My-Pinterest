2a900bba69a5440c184b6ddcf01c3ccec15f8139
PETIT <armand_petit@outlook.fr>
2025-03-15 20:04:46 +0100

    Ajout des différents écrans composants l'application : - welcome - home - image
    Ajout du BottomSheetModalProvider composant qui englobe l'application pour pouvoir utiliser correctement la BottomSheetModal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,34 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="home" />
-    </Stack>
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheetModalProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="home/index" />
+          <Stack.Screen
+            name="home/image"
+            options={{
+              presentation: "containedTransparentModal",
+              animation: "slide_from_bottom",
+              animationDuration: 200,
+            }}
+          />
+        </Stack>
+        <StatusBar style="light" />
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
